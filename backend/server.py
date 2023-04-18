d0b80100078fe760bbab6d6e25ce220231022fc1
Kelsey Bowen <kelseybowen22@gmail.com>
2023-04-18 14:27:08 -0700

    initial project setup

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,10 @@
 from flask_app import app
+from flask_cors import CORS
+
+api_cors_config = {
+    "origins": ["http://localhost:3000"]
+}
+CORS(app, resources={"/*": api_cors_config})
 
 if __name__ == "__main__":
     app.run(debug=True)
