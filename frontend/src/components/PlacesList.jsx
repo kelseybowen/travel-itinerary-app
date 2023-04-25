658090398f89afba039b8ba1f5dfb1ed9271f8f7
Kelsey Bowen <kelseybowen22@gmail.com>
2023-04-24 17:27:17 -0700

    front end updates. edit not working fully

--- frontend/src/components/PlacesList.jsx
+++ frontend/src/components/PlacesList.jsx
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const PlacesList = (props) => {
 
     const {userId, tripId} = useParams();
-    const {tripData, setTripData} = props;
+    const {tripData, setTripData, placeData, setPlaceData, isEdit, setIsEdit, setHeading, setButtonText} = props;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/dashboard/${userId}/plan/${tripId}`)
+            .then(res => {
+                console.log(res.data.data[0])
+                // setTripData(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     const handleDelete = (placeId) => {
-        axios.get(`http://localhost:5000/${userId}/trips/${tripId}/${placeId}/delete`)
+        axios.get(`http://localhost:5000/dashboard/${userId}/plan/${tripId}/${placeId}/delete`)
         .then(res => {
-            setTripData(res.data);
-            window.location.href=(`/dashboard/${userId}/plan/${tripId}`)
+            removeFromDom(placeId);
         })
         .catch(err => console.log(err))
     }
 
     const handleEdit = (placeId) => {
-        axios.get(`http://localhost:5000/${userId}/trips/${tripId}/${placeId}/edit`)
+        axios.get(`http://localhost:5000/dashboard/${userId}/plan/${tripId}/${placeId}/edit`)
         .then(res => {
-            setTripData(res.data);
-            window.location.href=(`/dashboard/${userId}/plan/${tripId}`)
+            // setTripData(res.data);
+            setPlaceData(res.data.data)
+            // console.log(`place dataaaaaa ${placeData.name}`)
+            // console.log(placeData)
+            // placeId = placeData.id
+            setIsEdit(true)
+            setHeading("Edit Place")
+            setButtonText("Update")
+            navigate(`/dashboard/${userId}/plan/${tripId}/${placeId}`)
         })
         .catch(err => console.log(err))
+        console.log(placeId)
+        navigate(`/dashboard/${userId}/plan/${tripId}/${placeId}`)
     }
+
+    const removeFromDom = placeId => {
+        setTripData(tripData.filter(place => place.id !== placeId))
+    }
+
     return (
         <div className='dashboard-component p-3 m-2'>
             <h2 className='text-center'>Places</h2>
@@ -41,7 +64,7 @@ const PlacesList = (props) => {
                     tripData.map((place) => {
                         return (
                             <tr key={place.id}>
-                                <td><Link to={`/dashboard/${userId}/${tripId}`}>{place.name}</Link></td>
+                                <td><Link to={"#"}>{place.name}</Link></td>
                                 <td>{place.address}</td>
                                 <td>{place.notes}</td>
                                 <td>
